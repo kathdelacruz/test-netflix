a24030b9f8d5b6be20981f6be08b73c893d0cde3
Katherine <katherine.delacruz8@gmail.com>
2017-03-16 23:43:08 -0500

    Integrate with local api

--- views/principalController.js
+++ views/principalController.js
@@ -4,21 +4,19 @@ angular
 
 PrincipalController.$inject = ['$scope', '$state', '$location', '$http'];
 function PrincipalController($scope, $state, $location, $http) {
-  console.log('hola');
 
-  $scope.activate = activate;
+  activate();
 
   function activate(name) {
-    console.log(name);
-    var url = 'http://www.omdbapi.com/?t=' + name;
+    var url = 'http://192.168.99.100:3000/films';
     $http({
       method: "GET",
       url: url
     })
     .then(function(response) {
       if(response.status===200){
-        $scope.data = response.data;
-        console.log($scope.data);
+        console.log(response.data);
+        $scope.dataList = response.data;
       } else {
         console.log('Error en la consulta');
       };
